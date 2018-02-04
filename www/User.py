d2d930ff3e3f092134e636a1cb055bd81820944e
vwozhaoxin <33049656+vwozhaoxin@users.noreply.github.com>
2018-02-04 10:01:35 +0800

    date2

--- www/User.py
+++ www/User.py
@@ -1,3 +1,17 @@
+#*-* coding:utf-8
+import orm
+from models import User, Blog, Comment
+import asyncio
+
+loop = asyncio.get_event_loop()
+async def test():
+    #await orm.create_pool(user='www-data', password='www-data', db='awesome', loop = loop)
+    await orm.create_pool(loop=loop, host='127.0.0.1', port=3306, user='www-data', password='www-data', db='awesome')
+    u = User(name='Test', email='test1@example.com', passwd='1234567890', image='about:blank',id ="1234")
+
+    await u.save()
+
+loop.run_until_complete(test())
 
 
 #create instance
--- www/orm.py
+++ www/orm.py
@@ -1,8 +1,8 @@
-# *-* config:utf-8 *-*
+# *-* coding:utf-8 *-*
 #author
 
 import logging;logging.basicConfig(level=logging.INFO)
-
+import aiomysql
 class ModelMetaclass(type):
 
     def __new__(cls, name, bases, attrs):
@@ -187,3 +187,53 @@ def create_args_string(num):
     for n in range(num):
         L.append('?')
     return ', '.join(L)
+
+
+def log(sql, args=()):
+    logging.info('SQL: %s' % sql)
+
+async def create_pool(loop, **kw):
+    logging.info('create database connection pool ...')
+    global __pool
+    __pool = await  aiomysql .create_pool(
+    host = kw.get('host', 'localhost'),
+    port = kw.get('port', '3306'),
+    user = kw['user'],
+    password = kw['password'],
+    db = kw['db'],
+    charset = kw.get('charset', 'utf8'),
+    autocommit = kw.get('autocommit', True),
+    maxsize = kw.get('maxsize', 10),
+    minsize = kw.get('minsize', 1)  ,
+    loop = loop
+    )
+async def select(sql, args, size = None):
+    log(sql, args)
+    global __pool
+    async with __pool.acquire() as conn:
+        async with conn.cursor(aiomysql.DictCursor) as cur:
+            await cur.execute(sql.replace('?',"%s"), args or ())
+            if size:
+                rs = await cur.fetchmany(size)
+            else:
+                rs = await cur.fetchall()
+
+        logging.info('rows returned: %s' % len(rs))
+        return rs
+
+async def execute(sql, args, autocommit = True):
+    log(sql)
+    async with __pool.acquire() as conn:
+        if not autocommit:
+            await conn.begin()
+        try:
+            async with conn.cursor(aiomysql.DictCursor) as cur:
+                    await cur.execute(sql.replace('?', '%s'),args)
+                    affected =cur.rowcount
+            if not autocommit:
+                await conn.commit()
+        except BaseException as e:
+            if not autocommit:
+                await conn.rollback()
+            raise
+        return affected
