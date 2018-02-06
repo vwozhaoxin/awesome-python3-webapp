db7405646a03c4936b88b0b721a30c4b6b1f4c03
vwozhaoxin <33049656+vwozhaoxin@users.noreply.github.com>
2018-02-06 20:44:07 +0800

    date8

--- www/app.py
+++ www/app.py
@@ -40,7 +40,7 @@ async def init(loop):
         logger_factory, response_factory
     ])
     init_jinja2(app, filters=dict(datetime=datetime_filter))
-    add_routes(app, 'testview')
+    add_routes(app, 'handlers')
     add_static(app)
     srv = await loop.create_server(app.make_handler(), '127.0.0.1', 9000)
     logging.info('server started at http://127.0.0.1:9000...')
--- www/handlers.py
+++ www/handlers.py
@@ -1,4 +1,35 @@
 # -*- coding: utf-8 -*-
 
 
-' url handlers '
+#' url handlers '
+from models import *
+from coroweb import get
+
+
+@get('/')
+def index(request):
+    summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    blogs = [
+        Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
+        Blog(id='2', name='Something New', summary=summary, created_at=time.time()-3600),
+        Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time()-7200)
+    ]
+    return {
+        '__template__': 'blogs.html',
+        'blogs': blogs
+    }
+
+
+
+
+@get('/hello')
+async def hello(request):
+    return '<h1>hello!</h1>'
+
+@get('/test')
+async def test(request):
+    users = await User.findAll()
+    return {
+        '__template__': 'test.html',
+        'users': users
+    }
